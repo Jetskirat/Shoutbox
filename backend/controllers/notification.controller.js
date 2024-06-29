ceb9511e0beb56b71b3f8808a1c936753ceed8c4
Jetskirat <jaskiratjsk1@gmail.com>
2024-06-30 02:56:15 +0530

    more error fixing (notifications)

--- backend/controllers/notification.controller.js
+++ backend/controllers/notification.controller.js
@@ -1,34 +1,32 @@
 import Notification from "../models/notification.model.js";
 
 export const getNotifications = async (req, res) => {
-    try{
-        const userId= req.user._id;
-        const notifications= await Notification.find({to: userId})
-        .populate({
-            path: "from",
-            select: "username profileImg",
-        });
+	try {
+		const userId = req.user._id;
 
-        await Notification.updateMany({to: userId, read: false}, {read: true});
+		const notifications = await Notification.find({ to: userId }).populate({
+			path: "from",
+			select: "username profileImg",
+		});
 
-        res.status(200).json({notifications});
-    }
-    catch{
-        console.log("Error in getNotifications controller: ", error.message);
-        res.status(500).json({error: error.message});
-    }
+		await Notification.updateMany({ to: userId }, { read: true });
+
+		res.status(200).json(notifications);
+	} catch (error) {
+		console.log("Error in getNotifications function", error.message);
+		res.status(500).json({ error: "Internal Server Error" });
+	}
 };
 
 export const deleteNotifications = async (req, res) => {
-    try{
-        const userId= req.user._id;
+	try {
+		const userId = req.user._id;
 
-        await Notification.deleteMany({to: userId});
+		await Notification.deleteMany({ to: userId });
 
-        res.status(200).json({message: "Notifications deleted"});
-    }
-    catch(error){
-        console.log("Error in deleteNotifications controller: ", error.message);
-        res.status(500).json({error: error.message});
-    }
+		res.status(200).json({ message: "Notifications deleted successfully" });
+	} catch (error) {
+		console.log("Error in deleteNotifications function", error.message);
+		res.status(500).json({ error: "Internal Server Error" });
+	}
 };
